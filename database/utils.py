f0795c5356c605566355ae9e3d1f141808a741b6
sambell <bell137@gmail.com>
2022-11-13 15:06:09 -0600

    make more generic

---
+++ database/utils.py
@@ -0,0 +1,62 @@
+import settings
+from dataclasses import dataclass
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import structlog
+
+from models.responses.healthcheck import DBCheck
+
+logger = structlog.get_logger("APP")
+
+
+@dataclass
+class DatabaseConfig:
+    dialect: str
+    user: str
+    password: str
+    host: str
+    port: str
+    db: str
+
+    @property
+    def connection_str(self):
+        return (
+            f'{self.dialect}://'
+            f'{self.user}:'
+            f'{self.password}@'
+            f'{self.host}:'
+            f'{self.port}/'
+            f'{self.db}'
+        )
+
+
+def get_postgres_conn_str():
+    return DatabaseConfig(
+        settings.PG_DIALECT,
+        settings.PG_USER,
+        settings.PG_PWD,
+        settings.PG_HOST,
+        settings.PG_PORT,
+        settings.PG_DB
+    ).connection_str
+
+
+def get_db_session(conn_str):
+    engine = create_engine(conn_str, pool_pre_ping = True)
+    session = sessionmaker()
+    session.configure(bind=engine)
+    return session()
+
+
+def validate_db_connection(db_session, database_name=None):
+    success = False
+    try:
+        db_session.connection()
+        success = True
+    except Exception as e:
+        logger.error(f'Unsuccessful connection', db_name=database_name, error=str(e))
+    finally:
+        return DBCheck(
+            available=success,
+            name=database_name
+        )
--- routers/system.py
+++ routers/system.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from models.responses.healthcheck import DBCheck, System
-from database import pg
 from settings import PG_DB, APP_NAME
+from database.utils import validate_db_connection, get_postgres_conn_str, get_db_session
 
 router = APIRouter(
     prefix="/system",
@@ -10,12 +10,10 @@ router = APIRouter(
 
 
 @router.get("/dbcheck", response_model=DBCheck, response_model_exclude_unset=True)
-def check_db_connection():
-    successful = pg.validate_pg_connection()
-    return DBCheck(
-        available=successful,
-        details="unable to connect; check connection string" if not successful else None,
-        name=PG_DB
+def check_postgres_connection():
+    return validate_db_connection(
+        db_session=get_db_session(get_postgres_conn_str()),
+        database_name=PG_DB
     )
 
 
