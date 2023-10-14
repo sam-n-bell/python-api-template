dda5b9ceae83f6d6809165da9947d043fa3ee3b9
Sam Bell <bell137@gmail.com>
2023-10-14 13:33:38 -0500

    more updates to sh file

--- name_replacer.sh
+++ name_replacer.sh
@@ -1,8 +1,7 @@
 NAME=$1
 python project_setup.py "$NAME"
-cd ..
-cd $NAME
+cd ../$NAME
 find . -type f \( -name "*.yaml" -o -name "*.py" \) -exec sed -i "s/{{template_name}}/\L$NAME/g" {} +
 find . -type f \( -name "*.md" \) -exec sed -i "s/{{YourAppName}}/\L$NAME/g" {} +
-echo "Start coding"
+echo "New project directory $NAME created. Start coding."
 sleep 5
